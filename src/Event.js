d8b8f2cab920ea404a1c8bc93a4f6ae4d2a7a968
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-12 15:49:06 +0800

    act2

---
+++ src/Event.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function Event(){
+    const[name,setName] = useState('');
+
+    const handleClick = () => {
+        alert(`${name}`);
+    }
+
+    return(
+        <div>
+            <button onClick={handleClick}>Submit</button>
+            <label>Name: <input type = "text" value={name} onChange={(e) => setName(e.target.value)}/></label>
+            <p>Your Name is: {name} </p>
+        </div>
+    );
+}
+
+export default Event;
